12441c16b5e07ec520e402950cf314d744845824
facundocarballo <carballofacundo70@gmail.com>
2025-12-05 11:38:15 -0300

    feat: repeated-string

--- implementation/repeated-string/solution.py
+++ implementation/repeated-string/solution.py
@@ -1,1 +1,7 @@
-print("Hello World")
+def repeatedString(s, n):
+    a_count = s.count('a')
+    complete_word_in_n = int(n / len(s))
+    mod_of_the_rest_word = n % len(s)
+    a_count_in_mod = s[:mod_of_the_rest_word].count('a')
+    
+    return a_count * complete_word_in_n + a_count_in_mod
